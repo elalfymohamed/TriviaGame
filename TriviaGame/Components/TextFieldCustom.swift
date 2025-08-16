14cb46bef691e57b5a0dd6f4e31fe9253728c2b6
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-08-17 01:40:29 +0300

    Create TextFieldCustom.swift

---
+++ TriviaGame/Components/TextFieldCustom.swift
@@ -0,0 +1,41 @@
+//
+//  TextFieldCustom.swift
+//  TriviaGame
+//
+//  Created by Alfy on 16/08/2025.
+//
+
+import SwiftUI
+
+struct TextFieldCustom: View {
+    @Binding var value: String
+    
+    var body: some View {
+            VStack(spacing: 5){
+               
+                   
+                TextField("Player ...",
+                          text: $value,
+                     )
+                .textInputAutocapitalization(.words)
+                .padding(10)
+                .overlay(
+                  RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color("AccentColor"), lineWidth: 1)
+                     )
+                .onChange(of: value) { newValue in
+                    if newValue.count > 20 {
+                        value = String(newValue.prefix(20))
+                    }
+                }
+
+                
+            }
+            .padding()
+        }
+        
+}
+
+//#Preview {
+//    TextFieldCustom()
+//}
