4169d64cc32872b3d59bb6413a3e0095cd2f599d
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-08-16 03:02:22 +0300

    Update ContentView.swift

--- TriviaGameDemo/Views/ContentView.swift
+++ TriviaGameDemo/Views/ContentView.swift
@@ -8,26 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var triviaManager = TriviaManager()
+    
     var body: some View {
-        VStack(spacing: 40) {
-            VStack(spacing: 20) {
-                Text("Traivia Game")
-                    .customLilacTitle()
-                    .monospaced(true)
+        NavigationView{
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text("Traivia Game")
+                        .customLilacTitle()
+                        .monospaced(true)
+                    
+                    Text("Are you ready to test out your trivia skills?")
+                        .font(.system(size: 17, weight: .medium, design: .default))
+                        .foregroundColor(Color("AccentColor"))
+                        .padding(.vertical, 0)
+                    
+                }
                 
-                Text("Are you ready to test out your trivia skills?")
-                    .font(.system(size: 17, weight: .medium, design: .default))
-                    .foregroundColor(Color("AccentColor"))
-                    .padding(.vertical, 0)
-                    
-                    
-
+                NavigationLink{
+                    TriviaView()
+                        .environmentObject(triviaManager)
+                }label:{
+                    PrimaryButton(text: "Let's Go!")
+                }
+                
             }
-            
-            
-            PrimaryButton(text: "Let's Go!")
+            .customVStackStyle()
         }
-        .customVStackStyle()
       
     }
 }
