72a29a6eb4022a7c83a9bb602aab8de177c4dfc0
elalfymohamed <elalfy.mohamed19@gmail.com>
2025-08-29 22:48:57 +0300

    Update StartGameView.swift

--- TriviaGame/Views/StartGameView.swift
+++ TriviaGame/Views/StartGameView.swift
@@ -9,49 +9,54 @@ import SwiftUI
 
 struct StartGameView: View {
     @StateObject var triviaManager = TriviaManager()
-    private let playerName:String = UserDefaults.standard.string(forKey: "playerName") ?? "player"
+    var playerName: String
 
     var body: some View {
         NavigationView{
-            VStack(spacing: 40) {
-              
-                    Text("Trivia Game")
-                        .customLilacTitle(size: 20)
-                        .padding()
-                        .monospaced(true)
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
+
+            VStack {
+                Text("Trivia Game")
+                    .customLilacTitle(size: 20)
+                    .padding()
+                    .monospaced(true)
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+            
+                Spacer()
                 
-               
-                
-                VStack(spacing: 20) {
-                    Text("Hi \(playerName) 👋🏻")
-                        .customLilacTitle()
+                VStack(spacing: 30){
+                    VStack(spacing: 20) {
+                        Text("Hi \(playerName) 👋🏻")
+                            .customLilacTitle()
+                        
+                        Text("Are you ready to test out your trivia skills?")
+                            .font(.system(size: 18, weight: .medium, design: .default))
+                            .foregroundColor(Color("AccentColor"))
+                            .padding(.vertical, 0)
+                        
+                    }
                     
-                   
-                    
-                    Text("Are you ready to test out your trivia skills?")
-                        .font(.system(size: 18, weight: .medium, design: .default))
-                        .foregroundColor(Color("AccentColor"))
-                        .padding(.vertical, 0)
-                    
+                    NavigationLink{
+                        TriviaView()
+                            .environmentObject(triviaManager)
+                    }label:{
+                        PrimaryButton(text: "Let's Go!")
+                    }
                 }
                 
-                NavigationLink{
-                    TriviaView()
-                        .environmentObject(triviaManager)
-                }label:{
-                    PrimaryButton(text: "Let's Go!")
-                }
+                Spacer()
                 
             }
+            
             .customVStackStyle()
            
         }
-        .navigationBarHidden(true)
+        .padding(.vertical, 1)
+        .navigationBarBackButtonHidden(true)
+        
     }
     
 }
 
 #Preview {
-    StartGameView()
+    StartGameView(playerName: "Player")
 }
